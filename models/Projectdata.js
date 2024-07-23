c7879a7c326a0eb8c957db9b0bc2aa0318d7b439
romssprogrammer <60788468+romssprogrammer@users.noreply.github.com>
2024-07-23 20:21:10 +0200

    add project aphrodite

--- models/Projectdata.js
+++ models/Projectdata.js
@@ -1,9 +1,24 @@
 export const projectData = [
   {
     projectName: "AnimationStudionWise",
-    description: "AnimationWise",
+    description: "AnimationStudionWise",
     projectimage: "./images/animationsStudoWise.png",
     technologies: [{ tech: "ReactJS", logo: "" }],
+  },
+  {
+    projectName: "Godot Link",
+    description:
+      "Base on a 3 tiers architecture with a client implement in GDscript godot engine language, a webservice implemented in java and MySql Db.",
+    projectimage: "./images/godotlink.jpg",
+    technologies: [
+      { tech: ["Godot Engine", " ,Java", " ,webservice REST API"], logo: "" },
+    ],
+  },
+  {
+    projectName: "AphroditeEssence",
+    description: "AphroditeEssence ",
+    projectimage: "./images/Scenemodel.png",
+    technologies: [{ tech: ["ReactJS"], logo: "" }],
   },
   {
     projectName: "CameraPrices",
@@ -22,15 +37,6 @@ export const projectData = [
     description: "Chrome extension Todo list ",
     projectimage: "./images/todolistExtension.png",
     technologies: [{ tech: [" Chrome extension API"], logo: "" }],
-  },
-  {
-    projectName: "Godot Link",
-    description:
-      "Base on a 3 tiers architecture with a client implement in GDscript godot engine language, a webservice implemented in java and MySql Db.",
-    projectimage: "./images/godotlink.jpg",
-    technologies: [
-      { tech: ["Godot Engine", " ,Java", " ,webservice REST API"], logo: "" },
-    ],
   },
   {
     projectName: "SynergyDico",
